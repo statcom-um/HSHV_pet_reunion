032338b0a2b7f5560524e9a4a0b8715bb51f586c
lutiffan <lutiffan@umich.edu>
2025-02-14 12:59:16 -0500

    Update app_type_new1.py

--- streamlit/app_type_new1.py
+++ streamlit/app_type_new1.py
@@ -5,9 +5,9 @@ from folium.plugins import MarkerCluster
 import pandas as pd
 import requests
 from io import StringIO
-# trivial change
+
 # Function to load data from GitHub URL
-@st.cache
+st.cache_data
 def load_original_data():
     url = 'https://raw.githubusercontent.com/statcom-um/HSHV_pet_reunion/refs/heads/main/anran/Data/final_noduplicates.csv'
     try:
@@ -22,7 +22,6 @@ data = load_original_data()
 # Check if data is loaded successfully
 if data is not None:
     st.write("Data loaded successfully!")
-    st.write(data.head())  # Display the first few rows of the data
 
     # Add new species column
     data['Species_new'] = data['Species'].apply(lambda x: 'Cat' if x == 'Cat' else ('Dog' if x == 'Dog' else 'Others'))
