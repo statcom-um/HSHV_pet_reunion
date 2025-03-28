66cc7335fb763089958478565b177e9b88e8ddf7
Anran Yao <116683749+anranyao@users.noreply.github.com>
2025-03-28 12:33:34 -0400

    front page

---
+++ streamlit/front.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="HSHV streamlit-app",
+    page_icon=":world_map:️",
+    layout="wide",
+)
+
+"# HSHV streamlit-app"
+
+"""Intros"""
+
+"""
+Currently, there are 4 maps:
+
+- `map1`: 
+
+- `map2`: .
+"""
+
+"""
+Intros
+
+
+"""
