95f8413dddc64e1b34d20da18286a7c61f16cd7a
ishakhapre <98626197+ishakhapre@users.noreply.github.com>
2025-04-07 18:30:35 -0400

    Update Introduction.py

--- streamlit/Introduction.py
+++ streamlit/Introduction.py
@@ -13,23 +13,20 @@ st.markdown("##### Humane Society of Huron Valley (HSHV) | Data Insights")
 
 # Intro Section
 st.markdown("""
-Welcome to the Return-to-Owner Analysis dashboard.
 
 This interactive app presents insights derived from our recent analysis, aimed at understanding patterns and opportunities related to animal returns to their owners.
 
 """)
+
 st.markdown("---")
 
+st.title("View Full Report")
 st.download_button(
     label="Download Full Report",
     data=open("Results_final.pdf", "rb"),
     file_name="HSHV_RTO_Report.pdf",
     mime="application/pdf",
 )
-
-st.markdown("---")
-
-st.title("View Full Report")
 
 with open("Results_final.pdf", "rb") as f:
     base64_pdf = f.read()
