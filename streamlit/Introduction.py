77129d92983b57d85798949c0497228a8b8f7af0
ishakhapre <98626197+ishakhapre@users.noreply.github.com>
2025-04-07 17:58:22 -0400

    Update Introduction.py

--- streamlit/Introduction.py
+++ streamlit/Introduction.py
@@ -1,15 +1,3 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Return-to-Owner Analysis",
-    page_icon=":world_map:️",
-    layout="wide",
-)
-
-st.sidebar.success("Select a demo above.")
-
-"# HSHV streamlit-app"
-
 """Intros"""
 import streamlit as st
 from PyPDF2 import PdfReader
